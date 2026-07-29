316d0e32862f043146f9d269ed47a61bf907efbe
owen <ofairbairn63@gmail.com>
2026-07-29 11:47:16 -0700

    chore - adding new files preparing for adding

---
+++ Mapper/data_preprocess/data_scaler.py
@@ -0,0 +1,4 @@
+"""
+Data scaler for the Mapper. Uses scikit-learn's robust scaler to scale the data.
+The robust scaler is less sensitive to outliers than the standard scaler, making it a good choice for data with outliers.
+"""
---
+++ Mapper/pipeline.py
@@ -0,0 +1,3 @@
+"""
+mapper pipeline for surge models. This is the runner that will run the whole Mapper workflow.
+"""
